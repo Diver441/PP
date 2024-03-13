e566e23215466d9973e9fedee75bda26479d8c0e
Diver441 <dimon.kh275@gmail.com>
2024-03-13 18:24:06 +0100

    Hello Name

--- main_2.py
+++ main_2.py
@@ -1,2 +1,9 @@
-x= 1+9
-print (x)
+import datetime
+hour = datetime.datetime.now().hour
+if hour >=18 :
+    Gr = "Good Evening,"
+else :
+    Gr = "Guten Tag,"
+
+Name = input("What is your name? --- ", )
+print (Gr, Name)
